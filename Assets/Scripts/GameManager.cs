52da0335296c43e95a054ec27925a95c93b7fbc4
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2023-07-18 23:17:05 +0100

    Gameplay sending questions to players

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Realtime;
+using Photon.Pun;
+using TMPro;
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI recievedQuestion;
+    private QuestionGenerator questionGenerator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        questionGenerator = GetComponent<QuestionGenerator>();
+        Invoke("SendStringOverNetwork", 2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SendStringOverNetwork()
     {
-        
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonView photonView = PhotonView.Get(this);
+            string question = questionGenerator.RandomQuestions().question;
+            photonView.RPC("ReceiveString", RpcTarget.AllBuffered, question);
+        }
+       
+    }
+    
+    [PunRPC]
+    private void ReceiveString(string receivedMessage)
+    {
+        Debug.Log("Received message: " + receivedMessage);
+        recievedQuestion.text = receivedMessage;
+        // Do something with the received message
     }
 }
+
