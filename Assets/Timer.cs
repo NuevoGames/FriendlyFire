89ea3252ccc2ab458f78ae1ea6f117cb084e77f0
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2023-07-18 22:25:05 +0100

    Made timer functionality

---
+++ Assets/Timer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float timerDuration;
+
+    // this script is responsible for creating Timers
+
+
+    public void StartCountdown(float duration)
+    {
+        timerDuration = duration;
+        InvokeRepeating(nameof(UpdateCountdown), 0f, 1f);
+    }
+
+    private void UpdateCountdown()
+    {
+        timerDuration -= 1f;
+        Debug.Log("Time Remaining: " + timerDuration.ToString("0"));
+        if (timerText != null) {
+            timerText.text = timerDuration.ToString();
+        }
+
+        if (timerDuration <= 0f)
+        {
+            CancelCountdown();
+            Debug.Log("Countdown Finished!");
+        }
+    }
+
+    private void CancelCountdown()
+    {
+        CancelInvoke(nameof(UpdateCountdown));
+    }
+}
